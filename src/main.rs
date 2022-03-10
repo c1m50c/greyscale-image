bf794d89ea746bfcc1a54a806b9e1ef31a999649
c1m50c <terminalityms@gmail.com>
2022-03-09 21:20:19 -0600

    Rust Implementation with `image` Package ✔

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use image::GenericImageView;
+
+use std::vec::Vec;
+use std::env;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 2, "Too many arguments passed into the application.");
+
+    let image_path = args[1].clone();
+
+    let image = image::open(image_path.clone())
+        .expect(format!("Cannot open an image from path {:?}.", image_path).as_str());
+    
+    if cfg!(debug_assertions) {
+        println!("Image Path {:?}", image_path);
+        println!("Dimensions {:?}", image.dimensions());
+        println!("ColorType {:?}", image.color());
+    }
+
+    let image = image.grayscale();
+    image.save("output.jpg")
+        .expect("Could properly save the image.");
 }
